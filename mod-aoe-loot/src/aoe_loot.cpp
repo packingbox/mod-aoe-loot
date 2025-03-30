9fd0608feb34f5f5684aefcff31be9587291ec5f
TerraByte <terrabyte@thebestwowprivateserver.com>
2025-03-30 09:32:08 -0500

    Update aoe_loot.cpp

--- mod-aoe-loot/src/aoe_loot.cpp
+++ mod-aoe-loot/src/aoe_loot.cpp
@@ -45,8 +45,7 @@ bool AOELootServer::CanPacketReceive(WorldSession* session, WorldPacket& packet)
 
             Loot* loot = &creature->loot;
 
-            LOG_DEBUG("module.aoe_loot", "Quest Items Size for {}: {}", creature->GetAIName(), loot->quest_items.size());
-            ChatHandler(player->GetSession()).PSendSysMessage(fmt::format("AOE Loot: Quest Items Size for {}: {}", creature->GetAIName(), loot->quest_items.size()));
+
             // Process quest items
             for (size_t i = 0; i < loot->quest_items.size(); ++i)
             {
@@ -56,8 +55,7 @@ bool AOELootServer::CanPacketReceive(WorldSession* session, WorldPacket& packet)
                 session->HandleAutostoreLootItemOpcode(autostorePacket);
             }
 
-            LOG_DEBUG("module.aoe_loot", "Regular Items Size for {}: {}", creature->GetAIName(), loot->items.size());
-            ChatHandler(player->GetSession()).PSendSysMessage(fmt::format("AOE Loot: Regular Items Size for {}: {}", creature->GetAIName(), loot->items.size()));
+
             // Process regular items
             for (size_t i = 0; i < loot->items.size(); ++i)
             {
@@ -67,8 +65,7 @@ bool AOELootServer::CanPacketReceive(WorldSession* session, WorldPacket& packet)
                 session->HandleAutostoreLootItemOpcode(autostorePacket);
             }
 
-            LOG_DEBUG("module.aoe_loot", "Gold Amount for {}: {}", creature->GetAIName(), loot->gold);
-            ChatHandler(player->GetSession()).PSendSysMessage(fmt::format("AOE Loot: Found {} gold on nearby corpse.", loot->gold));
+
             if (creature->loot.gold > 0)
             {
                 player->SetLootGUID(creature->GetGUID());
